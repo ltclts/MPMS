1f2fb9018328fd7a6952aaf762555f7ecb0cdedf
曹禺 <caoyu@shein.com>
2019-01-29 14:39:14 +0800

    卧槽 日志重新设定

--- services/log/log.go
+++ services/log/log.go
@@ -7,15 +7,20 @@ import (
 )
 
 func init() {
-	logs.EnableFuncCallDepth(true)
 	logs.Async(1e3)
+	logs.SetLogFuncCall(true)
+}
+
+func setLogger() {
 	_ = logs.SetLogger(logs.AdapterFile, fmt.Sprintf(`{"filename":"logs/%s/%s.log"}`, helper.NowDate(), helper.NowHour()))
 }
 
 func Info(f interface{}, v ...interface{}) {
+	setLogger()
 	logs.Info(f, v)
 }
 
 func Err(f interface{}, v ...interface{}) {
+	setLogger()
 	logs.Error(f, v)
 }
